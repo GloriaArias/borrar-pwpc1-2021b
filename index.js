f2b62ba3ba4a1667c6620bdda72cee60638d4440
GloriaArias <ariasutreragloria@gmail.com>
2021-11-04 12:50:51 -0600

    Sesión 22 Creando un servidor con http

---
+++ index.js
@@ -0,0 +1,27 @@
+//Importar el modulo http
+import http from 'http';
+
+//Crear el servidor
+//cb es una *función* que se ejecutará
+//ante cualquier petición de un recurso a nuestro server
+//(request, response)
+const server = http.createServer((req, res)=>{
+    console.log("> Se ha recibido una petición.");
+    //Respondemos
+    res.write('Hola');
+    //Terminamos la conexión
+    res.end();
+});
+
+// 3. Pongo a trabajar el servidor
+//le paso un callback que escribira en la consola
+//cuando el servidor este escuchando
+//192.168.56.1
+//192.168.0.31
+//192.168.127.1
+//192.168.126.1
+//192.168.56.1
+
+server.listen(3000, '192.168.56.1',()=>{
+    console.log("Servidor escuchando en http://192.168.56.1:3000");
+});
