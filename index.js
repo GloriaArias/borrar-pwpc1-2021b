1e797dca6773a2411d6d19e25949375afe7f0faa
GloriaArias <ariasutreragloria@gmail.com>
2021-11-09 02:17:00 -0600

    S24 Enrutado de Peticiones

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
-//Importar el modulo http
+// 1. Importar el modulo http
 import http from 'http';
 
-//Crear el servidor
+// 2. Crear el servidor
 //cb es una *función* que se ejecutará
 //ante cualquier petición de un recurso a nuestro server
 //(request, response)
-const server = http.createServer((req, res)=>{
-    console.log("> Se ha recibido una petición.");
-    //Registrar información de la petición
-    console.log(`Información de la petición`);
-    console.log(`url: ${req.url}`);
-    console.log(`Request Method: ${req.method}`);
-    //Establecer el tipo de contenido que se entregará al cliente
-    res.setHeader('Content-Type','text/html');
+    const server = http.createServer((req, res)=>{
+        //Obteniendo el recurso solicitado
+        let{ url, method } = req;
 
-    //Envío el contenido
-    res.write("<html>");
-    res.write("<head><title>My App</title></head>");
-    res.write(`<body><h1>Hello from the server &#128519;
-</h1><p style = "color:red">Recurso solicitado: ${req.url}</p></body>`);
-    res.write('</html>');
-    //Terminamos la conexión
-    res.end();
-});
+        //Informa en la consola del servidor que se recibe una petición 
+        console.log(`Se ha solicitado el siguiente recurso: ${method}: ${url}`);
 
+        //filtrar url
+        if(url === '/'){
+            //Respuesta ante "Get /"
+            // 1. Estableciendo el tipo de retorno como HTML
+            res.setHeader('Content-Type', 'text/html');
+            // 2. Escribiendo la respuesta
+            res.write('<html>')
+            res.write('<head><title>My App</title></head>');
+            res.write('<body><h1>&#9889; Hello from my server &#9889;</h1></body>')
+            res.write('</html>');
+            res.end();
+
+        }else{
+            //Se registra el Recurso no encontrado
+            console.log(`No se ha encontrado el recurso: ${url}`);
+            //Recuerso no encontrado 
+             // 1. Estableciendo el tipo de retorno como HTML
+             res.setHeader('Content-Type', 'text/html');
+             // 2. Escribiendo la respuesta
+             res.write('<html>')
+             res.write('<head><title>My App</title></head>');
+             res.write('<body><h1>Error: 404 - Recurso no encontrado &#9940;</h1></body>')
+             res.write('</html>');
+             res.end();     
+        }
+    });
+   
 // 3. Pongo a trabajar el servidor
 //le paso un callback que escribira en la consola
 //cuando el servidor este escuchando
 //192.168.56.1
-//192.168.0.31
-//192.168.127.1
-//192.168.126.1
-//192.168.56.1
-
-server.listen(3000, '192.168.56.1',()=>{
-    console.log("Servidor escuchando en http://192.168.56.1:3000");
+server.listen(3000, '0.0.0.0',()=>{
+    console.log("Servidor escuchando en http://0.0.0.0:3000");
 });
